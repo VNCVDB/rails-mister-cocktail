af838a3b2dad3c4e366bc2407ad36d762a523df6
Vincent Vandenborne <vncvdb@gmail.com>
2019-06-06 12:46:14 +0200

    logic and basic UI, still to add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   # end
   # resources :doses, only: [:destroy]
 
-  get "/cocktails",                         to: "cocktails#index"
-  get "/cocktails/new",                     to: "cocktails#new"
-  get "/cocktails/:id",                     to: "cocktails#show", as: "cocktail"
-  post "/cocktails",                        to: "cocktails#create"
-  get "/cocktails/:cocktail_id/doses/new",  to: "doses#new"
-  post "/cocktails/:cocktail_id/doses/",    to: "doses#create", as: "cocktail_doses"
-  delete "doses/:id",                       to: "doses#destroy"
+  get "/cocktails",                           to: "cocktails#index"
+  get "/cocktails/new",                       to: "cocktails#new",  as: "new_cocktail"
+  get "/cocktails/:id",                       to: "cocktails#show", as: "cocktail"
+  post "/cocktails",                          to: "cocktails#create"
+  get "/cocktails/:cocktail_id/doses/new",    to: "doses#new",      as: "new_cocktail_dose"
+  post "/cocktails/:cocktail_id/doses/",      to: "doses#create",   as: "cocktail_doses"
+  delete "/doses/:id",                        to: "doses#destroy",  as: "dose"
   # no route needed via cocktails on this last one,
   # we can delete without knowing the cocktail ID
   # cause we already know the dose ID to destroy.
